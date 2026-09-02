4ecff25ea462ef8bf27518398d2d9290b7b9ddfa
Apieceoftears <linm99685@gmail.com>
2026-09-02 13:34:38 -0400

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <windows.h>
+#include <mmdeviceapi.h>
+#include <audioclient.h>
+#include <shellapi.h>
+
+int WINAPI wWinmain(HINSTANCE hInstance,
+                    HINSTANCE hPrevInstance,
+                    PWSTR pCmdLine,
+                    int nCmdShow) {
+
+
+
+
+
+
+
+
+
+}
