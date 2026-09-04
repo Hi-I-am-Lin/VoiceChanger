f9e62056748dcc3e4e3514d948c67edbafe76807
Apieceoftears <linm99685@gmail.com>
2026-09-03 21:17:40 -0400

    initialized window

--- main.cpp
+++ main.cpp
@@ -3,17 +3,84 @@
 #include <audioclient.h>
 #include <shellapi.h>
 
-int WINAPI wWinmain(HINSTANCE hInstance,
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+int WINAPI wWinMain(HINSTANCE hInstance,
                     HINSTANCE hPrevInstance,
                     PWSTR pCmdLine,
-                    int nCmdShow) {
+                    int nCmdShow)
+{
+	
+    const wchar_t CLASS_NAME[] = L"Window Class";       // define name for the window class
+
+	    
+    WNDCLASS wc = { };                  // Create and initialize a WNDCLASS structure.
+
+    
+    wc.lpfnWndProc = WindowProc;        // Set the window procedure.
+    
+    wc.hInstance = hInstance;           // Set the application instance.
+    
+    wc.lpszClassName = CLASS_NAME;      // Set the window class name.
+
+    RegisterClass(&wc);                 // pass the address of the WNDCLASS structure to the RegisterClass function
 
 
+    HWND hwnd = CreateWindowEx(
+        0,                              // Optional window styles.
+        CLASS_NAME,                     // Window class
+        L"Learn to Program Windows",    // Window text
+        WS_OVERLAPPEDWINDOW,            // Window style
 
+        // Size and position
+        CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
 
+        NULL,       // Parent window    
+        NULL,       // Menu
+        hInstance,  // Instance handle
+        NULL        // Additional application data
+    );
 
+    if (hwnd == NULL)
+    {
+        return 0;
+    }
 
+    ShowWindow(hwnd, nCmdShow);
 
+    // Run the message loop.
 
+    MSG msg = { };
+    while (GetMessage(&msg, NULL, 0, 0) > 0)
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
 
+    return 0;
+}
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+    switch (uMsg)
+    {
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        return 0;
+
+    case WM_PAINT:
+    {
+        PAINTSTRUCT ps;
+        HDC hdc = BeginPaint(hwnd, &ps);
+
+        // All painting occurs here, between BeginPaint and EndPaint.
+
+        FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
+
+        EndPaint(hwnd, &ps);
+    }
+    return 0;
+
+    }
+    return DefWindowProc(hwnd, uMsg, wParam, lParam);
 }
